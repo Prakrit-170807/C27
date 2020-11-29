f51cefa91fd555a0769504542fadbbe32343224f
Prakrit Maan <prak170807@gmail.com>
2020-11-29 18:35:06 +0530

    this is a new file making the chain

---
+++ Chain.js
@@ -0,0 +1,22 @@
+class Chain {
+    constructor(bodyA,bodyB){
+        var options = {
+            bodyA : bodyA,
+            bodyB : bodyB,
+            stiffness : 0.1,
+            length : 8
+    
+        }
+    this.chain = Constraint.create(options)
+        World.add(world,this.chain)
+    }
+
+    display(){
+        var pointA = this.chain.bodyA.position
+        var pointB = this.chain.bodyB.position
+        push()
+        strokeWeight(5)
+        line(pointA.x,pointA.y,pointB.x,pointB.y)
+        pop()
+    }
+}
