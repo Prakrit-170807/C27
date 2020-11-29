5df75bde4e88ba69667e1aa7f9aad14a817220f2
Prakrit Maan <prak170807@gmail.com>
2020-11-29 18:34:14 +0530

    added a chain to be a new chain and it s outline

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+
+var log6,chain 
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,7 +37,15 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    log6 = new Log(150,150,70,PI/2)
+
     bird = new Bird(100,100);
+    
+    chain = new Chain(bird.body,log6.body)
+    
+
+    
+ 
 
 }
 
@@ -58,6 +69,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
+    log6.display();
+    chain.display()
 
     bird.display();
     platform.display();
